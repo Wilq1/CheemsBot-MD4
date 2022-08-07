e6df775153b1ae9aaff71b97790e7d799bf8d3f3
Wilq1 <110704969+Wilq1@users.noreply.github.com>
2022-08-07 16:20:05 -0300

    Update XeonCheems4.js

--- XeonCheems4.js
+++ XeonCheems4.js
@@ -3371,7 +3371,7 @@ case 'emojimix': {
 		let [emoji1, emoji2] = text.split`+`
 		let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
 		for (let res of anu.results) {
-		    let encmedia = await doctor.sendImageAsSticker(m.chat, res.url, m, { packname: global.packname, author: global.author, categories: res.tags })
+		    let encmedia = await XeonBotInt.sendImageAsSticker(m.chat, res.url, m, { packname: global.packname, author: global.author, categories: res.tags })
 		    await fs.unlinkSync(encmedia)
 		}
 	    }
